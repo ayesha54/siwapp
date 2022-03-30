38a0b0cccf984e7ef0d1b5750037ed40aa0f9ed7
hieumv0163 <hieumv0163@gmail.com>
2022-03-30 22:49:20 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,112 +1,112 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'dashboard#index'
+  # root 'dashboard#index'
 
-  get    'login'   => 'sessions#new',      as: :login
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy',  as: :logout
+  # get    'login'   => 'sessions#new',      as: :login
+  # post   'login'   => 'sessions#create'
+  # delete 'logout'  => 'sessions#destroy',  as: :logout
 
-  post 'test' => 'hooks#test'
+  # post 'test' => 'hooks#test'
 
-  get  'taxes/get_defaults', to: 'taxes#get_defaults'
+  # get  'taxes/get_defaults', to: 'taxes#get_defaults'
 
-  get  'services/get_defaults', to: 'services#get_defaults'
-  resources :taxes, :templates, :series, :services
-  get "invoices/amounts"
-  get "recurring_invoices/amounts"
-  get "items/amount"
+  # get  'services/get_defaults', to: 'services#get_defaults'
+  # resources :taxes, :templates, :series, :services
+  # get "invoices/amounts"
+  # get "recurring_invoices/amounts"
+  # get "items/amount"
   
-  resources :categories
-  resources :inventories
-  resources :beds
-  resources :rooms
-  get 'invoice/update_inventory', to: "inventories#update_inventory"
-  get '/bed/update_bed', to: "beds#update_bed"
-  get '/customer/update_content', to: "customers#update_content"
-  get '/invoice/update_content', to: "invoices#update_content"
-  get '/customer/get_by_id', to: "customers#get_by_identification"
-  get '/customer/update_cost', to: "customers#update_cost"
-  get '/tax/get_value', to: "taxes#get_tax_by_id"
+  # resources :categories
+  # resources :inventories
+  # resources :beds
+  # resources :rooms
+  # get 'invoice/update_inventory', to: "inventories#update_inventory"
+  # get '/bed/update_bed', to: "beds#update_bed"
+  # get '/customer/update_content', to: "customers#update_content"
+  # get '/invoice/update_content', to: "invoices#update_content"
+  # get '/customer/get_by_id', to: "customers#get_by_identification"
+  # get '/customer/update_cost', to: "customers#update_cost"
+  # get '/tax/get_value', to: "taxes#get_tax_by_id"
 
-  resources :commons do
-    post 'select_print_template', on: :member
-  end
-  resources :invoices do
-    post 'bulk', on: :collection
-    post 'select_print_template', on: :member
-    get 'autocomplete', on: :collection
-    get 'chart_data', on: :collection
-    get 'send_email', on: :member
-    get 'print', on: :member
-    get 'excel', on: :member
-  end
+  # resources :commons do
+  #   post 'select_print_template', on: :member
+  # end
+  # resources :invoices do
+  #   post 'bulk', on: :collection
+  #   post 'select_print_template', on: :member
+  #   get 'autocomplete', on: :collection
+  #   get 'chart_data', on: :collection
+  #   get 'send_email', on: :member
+  #   get 'print', on: :member
+  #   get 'excel', on: :member
+  # end
 
-  resources :recurring_invoices do
-    post 'generate', on: :collection
-    delete 'remove', on: :collection
-    get 'chart_data', on: :collection
-  end
+  # resources :recurring_invoices do
+  #   post 'generate', on: :collection
+  #   delete 'remove', on: :collection
+  #   get 'chart_data', on: :collection
+  # end
 
-  resources :customers do
-    get 'autocomplete', on: :collection
-    resources :invoices, only: [:index]
-    resources :recurring_invoices, only: [:index]
-    get 'send_email', on: :member
-    get 'print', on: :member
-    get 'excel', on: :member
-  end
+  # resources :customers do
+  #   get 'autocomplete', on: :collection
+  #   resources :invoices, only: [:index]
+  #   resources :recurring_invoices, only: [:index]
+  #   get 'send_email', on: :member
+  #   get 'print', on: :member
+  #   get 'excel', on: :member
+  # end
 
-  post 'templates/set_default', to: 'templates#set_default'
-  post 'series/set_default', to: 'series#set_default'
-  post 'taxes/set_default', to: 'taxes#set_default'
-  post 'services/set_default', to: 'services#set_default'
+  # post 'templates/set_default', to: 'templates#set_default'
+  # post 'series/set_default', to: 'series#set_default'
+  # post 'taxes/set_default', to: 'taxes#set_default'
+  # post 'services/set_default', to: 'services#set_default'
 
-  get 'settings/global'
-  put 'settings/global', to: 'settings#global_update'
-  get 'settings/profile'
-  put 'settings/profile', to: 'settings#profile_update'
-  get 'settings/tags'
-  put 'settings/tags', to: 'settings#tags_update'
-  get 'settings/hooks'
-  put 'settings/hooks', to: 'settings#hooks_update'
-  get 'settings/api_token'
-  post 'settings/api_token'
-  get 'excel', to: 'customers#excel_show'
-  post 'excel/import', to: 'invoices#import'
+  # get 'settings/global'
+  # put 'settings/global', to: 'settings#global_update'
+  # get 'settings/profile'
+  # put 'settings/profile', to: 'settings#profile_update'
+  # get 'settings/tags'
+  # put 'settings/tags', to: 'settings#tags_update'
+  # get 'settings/hooks'
+  # put 'settings/hooks', to: 'settings#hooks_update'
+  # get 'settings/api_token'
+  # post 'settings/api_token'
+  # get 'excel', to: 'customers#excel_show'
+  # post 'excel/import', to: 'invoices#import'
 
 
-  # API
-  namespace :api do
-    namespace :v1 do
-      get 'invoices/print_template/:id/invoice/:invoice_id', to: 'commons#print_template', as: :rendered_template
-      get 'stats', to: 'invoices#stats'
-      resources :taxes, :series, :services, only: [:index, :create, :show, :update, :destroy], defaults: { format: :json}
-      resources :customers, only: [:index, :create, :show, :update, :destroy], defaults: { format: :json} do
-        resources :invoices, only: [:index] # for filtering
-      end
-      resources :payments, only: [:show, :update, :destroy], defaults: {format: :json}
-      resources :items, only: [:show, :update, :destroy], defaults: {format: :json} do
-        resources :taxes, only: [:index], defaults: { format: :json}
-      end
-      resources :items, only: [:show, :update, :destroy], defaults: {format: :json} do
-        resources :services, only: [:index], defaults: { format: :json}
-      end
+  # # API
+  # namespace :api do
+  #   namespace :v1 do
+  #     get 'invoices/print_template/:id/invoice/:invoice_id', to: 'commons#print_template', as: :rendered_template
+  #     get 'stats', to: 'invoices#stats'
+  #     resources :taxes, :series, :services, only: [:index, :create, :show, :update, :destroy], defaults: { format: :json}
+  #     resources :customers, only: [:index, :create, :show, :update, :destroy], defaults: { format: :json} do
+  #       resources :invoices, only: [:index] # for filtering
+  #     end
+  #     resources :payments, only: [:show, :update, :destroy], defaults: {format: :json}
+  #     resources :items, only: [:show, :update, :destroy], defaults: {format: :json} do
+  #       resources :taxes, only: [:index], defaults: { format: :json}
+  #     end
+  #     resources :items, only: [:show, :update, :destroy], defaults: {format: :json} do
+  #       resources :services, only: [:index], defaults: { format: :json}
+  #     end
 
-      resources :invoices, only: [:index, :create, :show, :update, :destroy, :send_email], defaults: { format: :json} do
-        get 'send_email', on: :member
-        resources :items, :payments, only: [:index, :create]
-      end
+  #     resources :invoices, only: [:index, :create, :show, :update, :destroy, :send_email], defaults: { format: :json} do
+  #       get 'send_email', on: :member
+  #       resources :items, :payments, only: [:index, :create]
+  #     end
 
-      get 'recurring_invoices/generate_invoices', to: 'recurring_invoices#generate_invoices'
-      resources :recurring_invoices, only: [:index, :create, :show, :update, :destroy], defaults: { format: :json} do
-        resources :items, only: [:index, :create]
-      end
-    end
-  end
+  #     get 'recurring_invoices/generate_invoices', to: 'recurring_invoices#generate_invoices'
+  #     resources :recurring_invoices, only: [:index, :create, :show, :update, :destroy], defaults: { format: :json} do
+  #       resources :items, only: [:index, :create]
+  #     end
+  #   end
+  # end
 
-  localized do
-    resources :invoices, :recurring_invoices, :customers, :settings
-  end
+  # localized do
+  #   resources :invoices, :recurring_invoices, :customers, :settings
+  # end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
